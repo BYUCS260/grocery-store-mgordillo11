75a41af96d0dd1204cffd81176b8c94af0e4dcb3
Manuel <mgordillo11@outlook.com>
2022-03-06 19:49:13 -0700

    Working Lab

---
+++ grocery-store/src/views/CartView.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <h1>Your Cart</h1>
+    <div v-if="cartIsEmpty">
+      <p>You currently don't have any items in your shopping cart!</p>
+    </div>
+    <div v-else>
+      <CartList :products="cart" />
+    </div>
+  </div>
+</template>
+
+<script>
+import CartList from "../components/CartList.vue";
+export default {
+  name: "CartView",
+  components: {
+    CartList,
+  },
+  data() {
+    return {};
+  },
+  computed: {
+    cart() {
+      return this.$root.$data.cart;
+    },
+    cartIsEmpty() {
+      if (this.$root.$data.cart.length === 0) {
+        return true;
+      } else {
+        return false;
+      }
+    },
+  },
+  methods: {},
+};
+</script>
